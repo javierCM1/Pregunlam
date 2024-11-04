ccca3291abb93d753e675b4734d294dcdced35db
IvanLandin <ivan.landin24@gmail.com>
2024-11-04 18:31:24 -0300

    cambio RespuestaController

--- controller/RespuestaController.php
+++ controller/RespuestaController.php
@@ -23,8 +23,12 @@ class RespuestaController{
                 header("Location: /login");
                 exit();
             }
-            if (isset($_POST['continuar']) && $_POST['continuar'] === 'Respuesta Correcta') {
+            if (isset($_POST['continuar']) && $_POST['continuar'] && !$_SESSION['terminoPartida']) {
                 header("Location: /jugar");
+                exit();
+            }
+            if (isset($_POST['continuar']) && $_POST['continuar'] === false) {
+                header("Location: /lobby");
                 exit();
             }
 
@@ -37,26 +41,18 @@ class RespuestaController{
                 $this->preguntaModel->respondePregunta($partida['id_partida'], $pregunta['id_pregunta']);
 
             $this->preguntaModel->respuestaEsCorrecta($respuestaSeleccionada); //tira exception
-            $this->partidaModel->incrementarPuntajePartida($partida['id_partida'], 'a');
-            $this->preguntaModel->respondeCorrecto($partida['id_partida'], $pregunta['id_pregunta']);
-            $this->usuarioModel->incrementarPreguntasCorrectasUsuario($usuario['id_usuario']);
-            $this->preguntaModel->incrementarCantCorrectas($pregunta['id_pregunta']);
 
-            $data['message'] = 'Respuesta Correcta';
-            $data['usuario'] = $usuario;
-            $data['partida'] = $partida;
-            $data['pregunta'] = $pregunta;
-            $data['id_usuario'] = $usuario['id_usuario'];
-            $data['id_pregunta'] = $pregunta['id_pregunta'];
+            $_SESSION['usuario'] = $usuario;
+            $_SESSION['pregunta'] = $pregunta;
+            $_SESSION['partida'] = $partida;
 
-            $data['audio_src'] = 'public/music/WhatsApp Audio 2024-10-28 at 23.22.09.mpeg';
-            $data['respuestaEsCorrecta'] = true; // Establecer según la lógica
-            $this->presenter->show('resultadoPregunta', $data);
+            header('Location: /respuesta/correcta');
+            exit();
         }
         catch (PreguntaExpiradaException|RespuestaIncorrectaException $e) {
             $_SESSION['id_pregunta'] = $_POST['id_pregunta'];
             $_SESSION['message'] = $e->getMessage();
-            header('Location: /respuesta/respondeIncorrecto');
+            header('Location: /respuesta/incorrecta');
             exit();
         }
         catch (PartidaActivaNoExisteException $e) {
@@ -65,8 +61,36 @@ class RespuestaController{
             exit();
         }
     }
-    
-    public function respondeIncorrecto()
+
+    public function correcta()
+    {
+        try {
+            $usuario = $_SESSION['usuario'];
+            $partida = $_SESSION['partida'];
+            $pregunta = $_SESSION['pregunta'];
+
+            $this->partidaModel->incrementarPuntajePartida($partida['id_partida'], 'a');
+            $this->preguntaModel->respondeCorrecto($partida['id_partida'], $pregunta['id_pregunta']);
+            $this->usuarioModel->incrementarPreguntasCorrectasUsuario($usuario['id_usuario']);
+            $this->preguntaModel->incrementarCantCorrectas($pregunta['id_pregunta']);
+
+            $data['usuario'] = $usuario;
+            $data['pregunta'] = $pregunta;
+            $data['partida'] = $partida;
+            $data['id_usuario'] = $usuario['id_usuario'];
+            $data['id_pregunta'] = $pregunta['id_pregunta'];
+            $data['audio_src'] = 'public/music/WhatsApp Audio 2024-10-28 at 23.22.09.mpeg';
+            $data['respuestaEsCorrecta'] = true;
+            $this->presenter->show('resultadoPregunta', $data);
+
+        } catch (PartidaActivaNoExisteException $e) {
+            $_SESSION['message'] = $e->getMessage();
+            header('Location: /lobby');
+            exit();
+        }
+    }
+
+    public function incorrecta()
     {
         try {
             $usuario = $this->usuarioModel->getUserByUsernameOrEmail($_SESSION['user'], 'a');
@@ -74,7 +98,7 @@ class RespuestaController{
             $pregunta = $this->preguntaModel->obtenerPreguntaPorId($_SESSION['id_pregunta'], 2);
             $respuestaCorrecta = $this->preguntaModel->getRespuestaCorrectaDePregunta($pregunta['id_pregunta']);
             $_SESSION['terminoPartida'] = true;
-            
+
             $data['puntaje_final'] = $partida['puntaje_partida'];
             $data['message'] = $_SESSION['message'];
             $data['usuario'] = $usuario;
@@ -83,17 +107,13 @@ class RespuestaController{
             $data['id_usuario'] = $usuario['id_usuario'];
             $data['id_pregunta'] = $pregunta['id_pregunta'];
             $data['partida'] = $partida;
+            $data['respuestaEsCorrecta'] = false;
 
-            if (($_SESSION['message'] == 'Respuesta Incorrecta' || $_SESSION['message'] == 'Se terminó el tiempo')
-                && isset($_POST['continuar']))  {
-                $this->partidaModel->terminarPartida($partida['id_partida'], $usuario['id_usuario']);
-                $this->usuarioModel->determinarPuntajeMaximo($usuario, $partida);
-                header("Location: /lobby");
-                exit();
-            }
-            
+            $this->partidaModel->terminarPartida($partida['id_partida'], $usuario['id_usuario']);
+            $this->usuarioModel->determinarPuntajeMaximo($usuario, $partida);
+
             $this->presenter->show("resultadoPregunta", $data);
-            
+
         } catch (PartidaActivaNoExisteException $e) {
             $_SESSION['message'] = $e->getMessage();
             header('Location: /lobby');
@@ -115,6 +135,11 @@ class RespuestaController{
 
         $this->reporteModel->guardarReporte($motivo_reporte, $fecha_reporte, $id_usuario, $id_pregunta);
         $this->reporteModel->establecerPreguntaReportada($id_pregunta);
-        header("Location: /lobby");
+        if($_POST['continuar']){
+            header('Location: /respuesta');
+            exit();
+        }
+        header('Location: /lobby');
+        exit();
     }
 }
