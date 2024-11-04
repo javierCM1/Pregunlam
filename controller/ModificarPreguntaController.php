fa7dace4bb57ba101ee23892e2915fa27380c319
IvanLandin <ivan.landin24@gmail.com>
2024-11-04 13:45:38 -0300

    valido campo not empty

--- controller/ModificarPreguntaController.php
+++ controller/ModificarPreguntaController.php
@@ -20,7 +20,8 @@ class ModificarPreguntaController
         }
 
         $username = $_SESSION['user'];
-        $pregunta = $this->preguntaModel->obtenerPreguntaPorId($_GET['id'],$_GET['estado']);
+        $estado = $_GET['estado'];
+        $pregunta = $this->preguntaModel->obtenerPreguntaPorId($_GET['id'],$estado);
         $respuestaCorrecta = $this->preguntaModel->getRespuestaCorrectaDePregunta($pregunta['id_pregunta']);
 
         $respuestasIncorrectas = array_filter($this->preguntaModel->getRespuestasPorIdPregunta($pregunta['id_pregunta']),
@@ -32,23 +33,33 @@ class ModificarPreguntaController
         $incorrecta1 = $respuestasIncorrectas[0];
         $incorrecta2 = $respuestasIncorrectas[1];
         $incorrecta3 = $respuestasIncorrectas[2];
+        $message = $_SESSION['message'] ?? '';
 
         $this->presenter->show("modificarPregunta", ['username' => $username,
                                                     'pregunta' => $pregunta,
+                                                    'estado' => $estado,
                                                     'correcta' => $respuestaCorrecta,
                                                     'incorrecta1' => $incorrecta1,
                                                     'incorrecta2' => $incorrecta2,
-                                                    'incorrecta3' => $incorrecta3]);
+                                                    'incorrecta3' => $incorrecta3,
+                                                    'message' => $message]);
+        unset($_SESSION['message']);
     }
 
     public function guardarPreguntamodificada() {
 
-        if (isset($_POST['id_pregunta'], $_POST['pregunta'],
-                $_POST['id_respuestaCorrecta'], $_POST['respuestaCorrecta'],
-                $_POST['id_respuestaIncorrecta1'], $_POST['respuestaincorrecta1'],
-                $_POST['id_respuestaIncorrecta2'], $_POST['respuestaincorrecta2'],
-                $_POST['id_respuestaIncorrecta3'], $_POST['respuestaincorrecta3'],
-                $_POST['id_categoria'])) {
+        //implementar InputFormatValidator?
+        if (!empty($_POST['id_pregunta']) &&
+            !empty($_POST['pregunta']) &&
+            !empty($_POST['id_respuestaCorrecta']) &&
+            !empty($_POST['respuestaCorrecta']) &&
+            !empty($_POST['id_respuestaIncorrecta1']) &&
+            !empty($_POST['respuestaincorrecta1']) &&
+            !empty($_POST['id_respuestaIncorrecta2']) &&
+            !empty($_POST['respuestaincorrecta2']) &&
+            !empty($_POST['id_respuestaIncorrecta3']) &&
+            !empty($_POST['respuestaincorrecta3']) &&
+            !empty($_POST['id_categoria'])) {
 
             $idPregunta = $_POST['id_pregunta'];
             $pregunta = $_POST['pregunta'];
@@ -73,7 +84,7 @@ class ModificarPreguntaController
             exit();
         } else {
             $_SESSION['message'] = 'Faltan datos en el formulario';
-            header('Location: /modificarPregunta');
+            header('Location: /modificarPregunta?id='.$_POST['id_pregunta'].'&estado='.$_POST['id_estado']);
             exit();
         }
     }
